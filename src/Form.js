cfddc50fbeec02828938a5dbc923d31ac01aac2c
pyrlyk <pyrlykvitaliy@gmail.com>
2020-09-30 14:29:03 +0300

    formState

--- src/Form.js
+++ src/Form.js
@@ -1,64 +1,39 @@
-import React, {Component} from 'react';
+import React, {useState} from "react";
 
-class Form extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                username: '',
-                comments: '',
-                topic: 'react'
-            }
-        }
+function Form() {
+    const [username, setUsername] = useState('');
+    const [comment, setComment] = useState('');
+    const [topic, setTopic] = useState({topic: 'react'});
 
-        handlerUsernameChange = (event) =>  {
-            this.setState({
-                username: event.target.value
-                })
-}
-        handlerCommentChange = (event) => {
-            this.setState({
-                comments: event.target.value
-            })
-        }
-        handlerTopicChange = (event) => {
-            this.setState({
-                topic: event.target.value
-            })
-        }
-        handlerSubmit = (event) =>{
-            alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
-        }
-        render() {
-        return (
-            <form onSubmit={this.handlerSubmit}>
-                <div>
-                    <label>Username</label>
-                    <input type = 'text'
-                           value={this.state.username}
-                           onChange={this.handlerUsernameChange}/>
-                </div>
-                <div>
-                    <label>Comments</label>
-                    <textarea
-                        value={this.state.comments}
-                        onChange={this.handlerCommentChange}>
-                    </textarea>
-                </div>
+    const handleSubmit = () =>{
+        alert(`${username} ${comment} ${topic}`)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Username</label>
+                <input type='text' value={username} onChange={() => setUsername()}/>
+            </div>
+
+            <div>
+                <label>Comments</label>
+                <textarea value={comment} onChange={() => setComment()}/>
+            </div>
+
             <div>
                 <label>Topic</label>
-                <select
-                    value={this.state.topic}
-                    onChange={this.handlerTopicChange}>
-                        <option value="react">React</option>
-                        <option value="angular">Angular</option>
-                        <option value="vue">Vue</option>
+                <select value={topic} onChange={() => setTopic()}>
+                    <option value="react">React</option>
+                    <option value="angular">Angular</option>
+                    <option value="vue">Vue</option>
                 </select>
             </div>
-                <button type = "submit">Submit</button>
-            </form>
 
-        );
-    }
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
+
 
 export default Form;

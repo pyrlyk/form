bed0e30d0b3500b03f90610f99cd3c899e03482d
pyrlyk <pyrlykvitaliy@gmail.com>
2020-10-15 14:28:37 +0300

    form

--- src/Form.js
+++ src/Form.js
@@ -3,40 +3,41 @@ import React, {useState} from "react";
 function Form() {
     const [username, setUsername] = useState('');
     const [comment, setComment] = useState('');
-    const [topic, setTopic] = useState({topic: 'react'});
+    const [topic, setTopic] = useState("angular");
 
     const handleSubmit = () =>{
         alert(`${username} ${comment} ${topic}`)
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Username</label>
+        <form onSubmit={handleSubmit} className = "form">
+            <div >
+                <label>Username:</label>
                 <input type='text'
                        value={username}
                        onChange={(event) => setUsername(event.target.value)}/>
             </div>
 
             <div>
-                <label>Comments</label>
+                <label>Comments:</label>
                 <textarea
                     value={comment}
                     onChange={(event) => setComment(event.target.value)}/>
             </div>
 
             <div>
-                <label>Topic</label>
-                <select value={topic}
+                <label>Topic:</label>
+                <select defaultValue={topic}
                         onChange={(event) => setTopic(event.target.value)}>
-                    <option value="react">React</option>
                     <option value="angular">Angular</option>
+                    <option value="react">React</option>
                     <option value="vue">Vue</option>
                 </select>
             </div>
 
-            <button type="submit">Submit</button>
+            <button className = "button" type="submit" value = "submit">Submit</button>            
         </form>
+
     )
 }
 

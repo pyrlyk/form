c4fee9756f88966205b1c21ddf8da5a946dfbaf5
pyrlyk <pyrlykvitaliy@gmail.com>
2020-10-15 17:57:47 +0300

    form

--- src/Form.js
+++ src/Form.js
@@ -1,32 +1,35 @@
 import React, {useState} from "react";
+import "./Form.css"
 
 function Form() {
     const [username, setUsername] = useState('');
     const [comment, setComment] = useState('');
     const [topic, setTopic] = useState("angular");
-
+ 
     const handleSubmit = () =>{
         alert(`${username} ${comment} ${topic}`)
     }
 
     return (
         <form onSubmit={handleSubmit} className = "form">
-            <div >
+            <div className = "input">
                 <label>Username:</label>
                 <input type='text'
+                        placeholder = "Enter your name"
                        value={username}
                        onChange={(event) => setUsername(event.target.value)}/>
             </div>
-
-            <div>
+            
+            <div className = "label">
                 <label>Comments:</label>
                 <textarea
                     value={comment}
+                    placeholder = "Enter your comment"
                     onChange={(event) => setComment(event.target.value)}/>
             </div>
 
             <div>
-                <label>Topic:</label>
+                <label className = "select">Topic:</label>
                 <select defaultValue={topic}
                         onChange={(event) => setTopic(event.target.value)}>
                     <option value="angular">Angular</option>
